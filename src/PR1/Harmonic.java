a72f993010f1b48a0084da85414a610ef82300f1
tabureto4ka <73781615+tabureto4ka@users.noreply.github.com>
2020-12-18 01:54:48 +0300

    Update Harmonic.java

--- src/PR1/Harmonic.java
+++ src/PR1/Harmonic.java
@@ -4,16 +4,16 @@ public class Harmonic {
     public static void main(String[] args) {
         System.out.print("    ");
         for (int i=0; i < 5; i ++){
-            System.out.print("1   "); // Вывод первого слагаемого,а так же численной части дроби
+            System.out.print("1   ");
         }
 
         System.out.println("");
         System.out.print("1");
         for (int i=0; i < 5; i++){
-            System.out.print(" + -"); // Вывод суммы,а так же дробного разделителя
+            System.out.print(" + -");
         }
 
-        System.out.println("-"); // Вывод дробного разделителя ещё раз*,для корректного отображения
+        System.out.println("-");
 
         System.out.print("    ");
         for (int i=2; i < 7; i++){
@@ -21,9 +21,3 @@ public class Harmonic {
         }
     }
 }
-
-/*
-for (int i = 1; i < 11; i++) {
-        System.out.println(1./i);
-}
- */
